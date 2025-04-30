9b1b6f994ebc9ff9cc8eb7c6d01d2fbee0c43a69
Your Name <your.email@example.com>
2025-04-30 15:55:16 +0700

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -3,107 +3,65 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+$secretariat = [
+    [
+        'id' => 1,
+        'name' => 'Dr. Jelfina C. Alouw',
+        'position' => 'Director General',
+        'country' => 'Indonesia',
+        'urlImage' => 'https://coconutcommunity.org/files/image/picture-dr-jelfina-c-alouw.jpg'
+    ],
+    [
+        'id' => 2,
+        'name' => 'Mr. Aluthwala Hewa Nuwan Chinthaka',
+        'position' => 'Deputy Director General',
+        'country' => 'Sri Lanka',
+        'urlImage' => 'https://coconutcommunity.org/files/image/picture-mr-aluthwala-hewa-nuwan-chinthaka.jpg'
+    ],
+    [
+        'id' => 3,
+        'name' => 'Klaudio D. Hosang',
+        'position' => 'Administrative & Finance Director',
+        'country' => 'Indonesia',        
+        'urlImage' => 'https://coconutcommunity.org/files/image/picture-klaudio-d-hosang.jpg'
+    ],
+    [
+        'id' => 4,
+        'name' => 'Alit Pirmansah',
+        'position' => 'Market & Statistics Director',
+        'country' => 'Indonesia',
+        'urlImage' => 'https://coconutcommunity.org/files/image/picture-alit-pirmansah.jpg'
+    ],
+    [
+        'id' => 5,
+        'name' => 'Otniel Sintoro',
+        'position' => 'Information & Publication Director',
+        'country' => 'Indonesia',
+        'urlImage' => 'https://coconutcommunity.org/files/image/picture-otniel-sintoro.jpg'
+    ],
+    [
+        'id' => 6,
+        'name' => 'Maria Widiastuti',
+        'position' => 'Secretary',
+        'country' => 'Indonesia',
+        'urlImage' => 'https://coconutcommunity.org/files/image/picture-maria-widiastuti.jpg'
+    ],
+
+];
+
 Route::get('/', function () {
     return view('home', ['vision' => 'Globally Sustainable, Resilient, Inclusive and highly Profitable Coconut Sector']);
 });
 
-Route::get('/about', function () {
-    $secretariat = [
-        [
-            'id' => 1,
-            'name' => 'Dr. Jelfina C. Alouw',
-            'position' => 'Director General',
-            'country' => 'Indonesia',
-            'urlImage' => 'https://coconutcommunity.org/files/image/picture-dr-jelfina-c-alouw.jpg'
-        ],
-        [
-            'id' => 2,
-            'name' => 'Mr. Aluthwala Hewa Nuwan Chinthaka',
-            'position' => 'Deputy Director General',
-            'country' => 'Sri Lanka',
-            'urlImage' => 'https://coconutcommunity.org/files/image/picture-mr-aluthwala-hewa-nuwan-chinthaka.jpg'
-        ],
-        [
-            'id' => 3,
-            'name' => 'Klaudio D. Hosang',
-            'position' => 'Administrative & Finance Director',
-            'country' => 'Indonesia',        
-            'urlImage' => 'https://coconutcommunity.org/files/image/picture-klaudio-d-hosang.jpg'
-        ],
-        [
-            'id' => 4,
-            'name' => 'Alit Pirmansah',
-            'position' => 'Market & Statistics Director',
-            'country' => 'Indonesia',
-            'urlImage' => 'https://coconutcommunity.org/files/image/picture-alit-pirmansah.jpg'
-        ],
-        [
-            'id' => 5,
-            'name' => 'Otniel Sintoro',
-            'position' => 'Information & Publication Director',
-            'country' => 'Indonesia',
-            'urlImage' => 'https://coconutcommunity.org/files/image/picture-otniel-sintoro.jpg'
-        ],
-        [
-            'id' => 6,
-            'name' => 'Maria Widiastuti',
-            'position' => 'Secretary',
-            'country' => 'Indonesia',
-            'urlImage' => 'https://coconutcommunity.org/files/image/picture-maria-widiastuti.jpg'
-        ],
-
-    ];
+Route::get('/about', function () use ($secretariat) {
+   
     return view('about',[
         'secretariat' => $secretariat
     ]);
 });
 
-Route::get('/secretariat/{id}', function ($id) {
-    $secretariat = [
-        [
-            'id' => 1,
-            'name' => 'Dr. Jelfina C. Alouw',
-            'position' => 'Director General',
-            'country' => 'Indonesia',
-            'urlImage' => 'https://coconutcommunity.org/files/image/picture-dr-jelfina-c-alouw.jpg'
-        ],
-        [
-            'id' => 2,
-            'name' => 'Mr. Aluthwala Hewa Nuwan Chinthaka',
-            'position' => 'Deputy Director General',
-            'country' => 'Sri Lanka',
-            'urlImage' => 'https://coconutcommunity.org/files/image/picture-mr-aluthwala-hewa-nuwan-chinthaka.jpg'
-        ],
-        [
-            'id' => 3,
-            'name' => 'Klaudio D. Hosang',
-            'position' => 'Administrative & Finance Director',
-            'country' => 'Indonesia',        
-            'urlImage' => 'https://coconutcommunity.org/files/image/picture-klaudio-d-hosang.jpg'
-        ],
-        [
-            'id' => 4,
-            'name' => 'Alit Pirmansah',
-            'position' => 'Market & Statistics Director',
-            'country' => 'Indonesia',
-            'urlImage' => 'https://coconutcommunity.org/files/image/picture-alit-pirmansah.jpg'
-        ],
-        [
-            'id' => 5,
-            'name' => 'Otniel Sintoro',
-            'position' => 'Information & Publication Director',
-            'country' => 'Indonesia',
-            'urlImage' => 'https://coconutcommunity.org/files/image/picture-otniel-sintoro.jpg'
-        ],
-        [
-            'id' => 6,
-            'name' => 'Maria Widiastuti',
-            'position' => 'Secretary',
-            'country' => 'Indonesia',
-            'urlImage' => 'https://coconutcommunity.org/files/image/picture-maria-widiastuti.jpg'
-        ],
-
-    ];
+Route::get('/secretariat/{id}', function ($id) use ($secretariat) {
+   
     $selectedSecretariat = Arr::first($secretariat, fn($sec) => $sec['id'] == $id);
    
 
